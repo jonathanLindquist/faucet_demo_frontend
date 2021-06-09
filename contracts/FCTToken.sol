987ee5415eb3af23f92068f5decb57d8f94b7536
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2021-06-09 15:53:21 -0500

    remove greeter contract and add FCTToken

---
+++ contracts/FCTToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED"
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FCTToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 10000 * (10**18));
+    }
+
+    function faucet(address recipient, uint256 amount) external {
+        _mint(recipient, amount);
+    }
+}
